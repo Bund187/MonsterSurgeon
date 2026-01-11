bba8bebae9e5ed0800441d9bc9b6e0afe60ecfba
Guillermo <gsiliutop@gmail.com>
2026-01-11 21:21:04 +0100

    Pistola de dardos
    Dispara y apunta correctamente. Hace una pequeña vibracion y suena.

---
+++ Assets/Scripts/Effects/ScreenShake.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    public AnimationCurve curve; //Esto hace que se cree en el inspector una curva. El eje vertical será la fuerza del shake y la horizontal el tiempo que dura el shake
+    public float duration = 1f;
+
+    public void StartShake(float duration_)
+    {
+        duration= duration_;
+        StartCoroutine("Shaking");
+    }
+
+    public IEnumerator Shaking()
+    {
+        Vector3 startPosition = transform.position;                                     //Almacenamos la posicion inicial de la camara para resetearla más tarde
+        float elapsedTime = 0f;                                                         
+
+        while (elapsedTime < duration)                                                   
+        {
+            elapsedTime += Time.deltaTime;                                              //Se va incrementando con el tiempo
+            float strength = curve.Evaluate(elapsedTime / duration);                    //Se evalua la parte del tiempo (eje horizontal) de la curva para lograr la fuerza
+            transform.position = startPosition + Random.insideUnitSphere * strength;    //Se suma la posicion de la camara por un punto aleatorio por la fuerza y se añade a la posicion actual de la camara (genera el movimiento de la camara)
+            yield return null;
+        }
+
+        transform.position = startPosition;
+        ResetShake();
+    }
+
+    private void ResetShake()
+    {
+        duration = 1f;
+    }
+}
--- Assets/Scripts/Player/DartShooter.cs
+++ Assets/Scripts/Player/DartShooter.cs
@@ -1,18 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class DartShooter : MonoBehaviour
 {
     [SerializeField] GameObject scopeCanvas;
+    [SerializeField] RectTransform rectAim;
+    [SerializeField] AudioSource audioS;
+    [SerializeField] AudioClip shotSound;
 
-    private bool isAiming;
+    private bool isAiming, canShoot;
+    private RectTransform initialPosition;
+
+    private void Start()
+    {
+        canShoot=true;
+        initialPosition = rectAim;
+    }
     public void Shoot()    
     {
-        if (isAiming)
+        if (isAiming && canShoot)
         {
             //Suena el disparo
             //Pequeño shake del aim
             //Disparo
+            audioS.PlayOneShot(shotSound);
             print("Se dispara la pistola de dardos");
+            StartCoroutine(Utils.ShakeUI(rectAim, -10f, 10f));
+            canShoot = false;
+            StartCoroutine(CoolDown());
         }
     }
 
@@ -20,10 +35,17 @@ public class DartShooter : MonoBehaviour
     {
         isAiming = true;
         scopeCanvas.SetActive(isAiming);
+        rectAim = initialPosition;
     }
     public void ReleaseAim()
     {
         isAiming = false;
         scopeCanvas.SetActive(isAiming);
+    }
+
+    private IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canShoot = true;
     }
 }
